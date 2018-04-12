85ca0fbae539f5477921fa1a3dfffebda90749a5
Katarzyna Jedrzejczak <katarzyna.jedrzejczak@op.pl>
2018-04-12 17:36:18 +0200

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -54,17 +54,7 @@ module.exports = {
 			}
 		]
 	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: 'client/index.html',
-			filename: 'index.html',
-  			inject: 'body'
-		}),
-		new webpack.optimize.UglifyJsPlugin(),
-		new OptimizeJsPlugin({
-  			sourceMap: false
-		})
-	],
+	plugins: plugins,
     resolve: {
         extensions: ['.js']
     }
